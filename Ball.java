f208e717a8a6a57619558a9b9730357a8b92727c
mani <m.angerhofer@ufl.edu>
2017-03-04 20:07:47 -0500

    added edge collision detection

--- Ball.java
+++ Ball.java
@@ -1,3 +1,4 @@
+//Implements Ball Class
 import java.awt.geom.*;
 import java.awt.*;
 
@@ -13,55 +14,74 @@ public class Ball extends ColorShape {
     
     // constructor
     public Ball() {
-	super(shape);
-	// set ball color
-	super.setFillColor(Color.RED);
-	super.setBorderColor(Color.RED);
-	// set initial values for x and y position and speed
-	xPos=300;
-	yPos=450;
-	xSpeed=0;
-	ySpeed=2;
+		super(shape);
+		// set ball color
+		super.setFillColor(Color.RED);
+		super.setBorderColor(Color.RED);
+		// set initial values for x and y position and speed
+		xPos=300;
+		yPos=450;
+		xSpeed=0;
+		ySpeed=2;
     }
+    
+    //move ball
     public void move() {
-	// move ball
-	xPos+=xSpeed;
-	yPos+=ySpeed;
-	// detect if ball should bounce off an edge
-	if(xPos==0||xPos==600){
-	    setXspeed(-xSpeed);
-	}
-	if(yPos<0){
-	    setYspeed(-ySpeed);
-	}
-	// update shape to new values
-	shape.setFrame(xPos, yPos, width, height);
+		xPos+=xSpeed;
+		yPos+=ySpeed;
+		// detect if ball should bounce off an edge
+		if(xPos==0||xPos==600){
+		    setXspeed(-xSpeed);
+		}
+		if(yPos<0){
+		    setYspeed(-ySpeed);
+		}
+		// update shape to new values
+		shape.setFrame(xPos, yPos, width, height);
     }
+    
+    //Set xSpeed
     public void setXspeed(int newSpeed) {
-	xSpeed = newSpeed;
+    	xSpeed = newSpeed;
     }
+    
+    //Set ySpeed
     public void setYspeed(int newSpeed) {
-	ySpeed = newSpeed;
+    	ySpeed = newSpeed;
     }
+    
+    //get x position
     public int getX() {
-	return xPos;
+    	return xPos;
     }
+    
+    //get y position
     public int getY() {
-	return yPos;
+    	return yPos;
     }
+    
+    //get ball width
     public int getWidth() {
-	return width;
+    	return width;
     }
+    
+    //get ball height
     public int getHeight() {
-	return height;
+    	return height;
     }
+    
+    //get xSpeed
     public int getxSpeed(){
-	return xSpeed;
+    	return xSpeed;
     }
+    
+    //get ySpeed
     public int getySpeed(){
-	return ySpeed;
+    	return ySpeed;
     }
+    
+    //get Shape type
     public Ellipse2D.Double getShape() {
-	return shape;
+    	return shape;
     }
 }
