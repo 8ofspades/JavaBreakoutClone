915937f0800f84190e2bf2280f499a9ab3611679
mani <m.angerhofer@ufl.edu>
2017-03-04 20:18:00 -0500

    changed brick sizes

--- Brick.java
+++ Brick.java
@@ -1,3 +1,4 @@
+//Implements Brick Class
 import java.awt.geom.*;
 
 public class Brick extends ColorShape {
@@ -6,22 +7,27 @@ public class Brick extends ColorShape {
     private int width = 10;
     private int height = 5;
     private Rectangle2D.Double shape;
+    
     // constructor
     public Brick(int x, int y, int w, int h) {
-	super(new Rectangle2D.Double(x, y, w, h));
-	//set brick x, y, width, and height
-	xPos=x;
-	yPos=y;
-	width=w;
-	height=h;
-	// update shape
-	shape = (Rectangle2D.Double)super.shape;
-	shape.setRect(xPos, yPos, width, height);
+		super(new Rectangle2D.Double(x, y, w, h));
+		//set brick x, y, width, and height
+		xPos=x;
+		yPos=y;
+		width=w;
+		height=h;
+		// update shape
+		shape = (Rectangle2D.Double)super.shape;
+		shape.setRect(xPos, yPos, width, height);
     }
+    
+    //get brick x pos
     public int getX() {
-	return xPos;
+    	return xPos;
     }
+    
+    //get brick shape
     public Rectangle2D.Double getShape() {
-	return shape;
+    	return shape;
     }
 }
